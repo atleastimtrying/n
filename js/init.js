e47a01ddb1153c9c7aa84cf43bc053bb1856e5f1
Anders Fisher <atleastimtrying@gmail.com>
2013-09-05 14:08:50 +0100

    require js added

--- js/init.js
+++ js/init.js
@@ -1,5 +1,14 @@
 window.N = {};
-
-$(function(){
-  N.game = new N.Game();
+require([
+  "libs/jquery",
+  "js/dice.js",
+  "js/game.js",
+  "js/ganger.js",
+  "js/player.js",
+  "js/start_screen.js",
+  "js/utils.js",
+  "js/weapons.js"], function(){
+  $(function(){
+    N.game = new N.Game();
+  });
 });
